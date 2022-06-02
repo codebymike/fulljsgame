db96495ce158518cbdedc319212b7b5f32f3c97a
Mike Thomas <mikethomas84@gmail.com>
2022-06-02 23:34:59 +0100

    input setup

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Player } from './player.js';
+import { InputHandler } from './input.js';
 
 window.addEventListener('load', () => {
     const canvas = document.getElementById("canvas1");
@@ -11,6 +12,7 @@ window.addEventListener('load', () => {
             this.width = width;
             this.height = height;
             this.player = new Player(this);
+            this.input = new InputHandler();
         }
         update(){
             this.player.update();
