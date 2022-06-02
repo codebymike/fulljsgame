50800f3b7b44afdfb874ef3dcb773a2981cd473a
Mike Thomas <mikethomas84@gmail.com>
2022-06-02 22:59:12 +0100

    player and animate loop

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { Player } from './player.js';
+
 window.addEventListener('load', () => {
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext("2d");
@@ -8,12 +10,21 @@ window.addEventListener('load', () => {
         constructor(width, height){
             this.width = width;
             this.height = height;
+            this.player = new Player(this);
         }
         update(){
 
         }
-        draw(){
-            
+        draw(context){
+            this.player.draw(context);
         }
     }
+
+    const game = new Game(canvas.width, canvas.height);
+
+    function animate(){
+         game.draw(ctx);
+         requestAnimationFrame(animate);
+    }
+    animate();
 });
