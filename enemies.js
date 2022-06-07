e00e76d91299447e582d5224ca875f1773580f1f
Mike Thomas <mikethomas84@gmail.com>
2022-06-07 21:17:45 +0100

    debug mode added

--- enemies.js
+++ enemies.js
@@ -20,6 +20,7 @@
         if( this.x + this.width < 0 ) this.markedForDeletion = true;
      }
      draw( context ){
+        if( this.game.debug ) context.strokeRect(this.x, this.y, this.width, this.height);
         context.drawImage(
             this.image,
             this.frameX * this.width,
