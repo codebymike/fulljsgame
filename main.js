8b3d9b87879605881545702ad12b66edc59288bd
Mike Thomas <mikethomas84@gmail.com>
2022-06-10 23:21:19 +0100

    game width update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import { UI } from './ui.js';
 window.addEventListener('load', () => {
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext("2d");
-    canvas.width = 500;
+    canvas.width = 900;
     canvas.height = 500;
 
     class Game {
